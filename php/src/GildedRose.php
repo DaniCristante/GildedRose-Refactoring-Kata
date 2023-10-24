51d0c063b4969f4cf55d37e2bd0e5bd13e38bb91
Daniel Cristante <daniel.cristante@gmail.com>
2023-10-24 18:42:23 +0200

    small refactoring + tests

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -6,6 +6,12 @@ namespace GildedRose;
 
 final class GildedRose
 {
+    public const MAX_QUALITY = 50;
+
+    public const AGED_BRIE = 'Aged Brie';
+    public const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+    public const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+    
     /**
      * @param Item[] $items
      */
@@ -17,51 +23,58 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->name != self::AGED_BRIE and $item->name != self::BACKSTAGE_PASSES) {
                 if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+                    if ($item->name != self::SULFURAS) {
+                        $item->quality = $this->decreaseQuality($item->quality);
                     }
                 }
             } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
+                $item->quality = $this->increaseQuality($item->quality);
+                if ($item->name == self::BACKSTAGE_PASSES) {
+                    if ($item->sellIn < 11) {
+                        $item->quality = $this->increaseQuality($item->quality);
+                    }
+                    if ($item->sellIn < 6) {
+                        $item->quality = $this->increaseQuality($item->quality);
                     }
                 }
+                
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
+            if ($item->name != self::SULFURAS) {
+                $item->sellIn = $this->decreaseSellIn($item->sellIn);
             }
 
             if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->name != self::AGED_BRIE) {
+                    if ($item->name != self::BACKSTAGE_PASSES) {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
+                            if ($item->name != self::SULFURAS) {
+                                $item->quality = $this->decreaseQuality($item->quality);
                             }
                         }
                     } else {
                         $item->quality = $item->quality - $item->quality;
                     }
                 } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                    $this->increaseQuality($item->quality);
                 }
             }
         }
+    }
+
+    private function decreaseQuality(int $quality): int {
+        return $quality - 1;
+    }
+
+    private function increaseQuality(int $quality): int {
+        if ($quality < self::MAX_QUALITY) {
+            return $quality + 1;
+        }
+    }
+
+    private function decreaseSellIn(int $sellIn): int {
+        return $sellIn - 1;
     }
 }
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,11 +10,94 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    /**
+     * @test
+     */
+    public function itReturnsItems(): void
     {
         $items = [new Item('foo', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        static::assertEquals('foo', $items[0]->name);
+    }
+
+    /**
+     * @test
+     */
+    public function itDecreasesQuality(): void {
+        $items = [new Item('foo', 0, 10)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        static::assertEquals(8, $items[0]->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function itDecresaesSellInDate(): void  {
+        $items = [new Item('foo', 10, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        static::assertEquals(9, $items[0]->sellIn);
+    }
+
+    /**
+     * @test
+     */
+    public function itDoesNotDecreaseQualityToNegativeValue(): void {
+        $items = [new Item('foo', 0, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        static::assertEquals(0, $items[0]->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function itDoesNotDecreaseSulfurasQuality(): void {
+        $items = [new Item(GildedRose::SULFURAS, 0, 20)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        static::assertEquals(20, $items[0]->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function itIncreasesQualityByTwoIfLessThanElevenDaysToSellLeft(): void {
+        $items = [new Item(GildedRose::BACKSTAGE_PASSES, 9, 20)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        static::assertEquals(22, $items[0]->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function itIncreasesQualityByThreeIfLessThanSixDaysToSellLeft(): void {
+        $items = [new Item(GildedRose::BACKSTAGE_PASSES, 4, 20)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        static::assertEquals(23, $items[0]->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function itAlwaysIncreasesAgedBrieQuality(): void {
+        $items = [new Item(GildedRose::AGED_BRIE, -1, 20)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        static::assertEquals(21, $items[0]->quality);
+    }
+
+    /**
+     * @test
+    */
+    public function itDecreasesQualityTwiceFasterWhenSellInDateExpires(): void {
+        $items = [new Item('any', -1, 20)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        static::assertEquals(18, $items[0]->quality);
     }
 }
